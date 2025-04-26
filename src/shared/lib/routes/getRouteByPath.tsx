5cb1070ce11a8c7eb1d06c138d5ff02bb09c4eb2
beforedisappear <roman23kl@gmail.com>
2025-04-26 20:34:30 +0300

    fix getByPathId

--- src/shared/lib/routes/getRouteByPath.tsx
+++ src/shared/lib/routes/getRouteByPath.tsx
@@ -3,13 +3,13 @@ import { match } from 'path-to-regexp';
 
 export const getRouteByPath = (pathname: string): AppRoutes => {
   for (const [routeKey, pattern] of Object.entries(routePatterns)) {
+    if (pattern === '*') continue;
+
     const matcher = match(pattern, { decode: decodeURIComponent });
 
     const matched = matcher(pathname);
 
-    if (matched) {
-      return routeKey as AppRoutes;
-    }
+    if (matched) return routeKey as AppRoutes;
   }
 
   return AppRoutes.NOT_FOUND;
