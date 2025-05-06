5bd3f77366179b15a9502bc7364b9c97f8e62462
beforedisappear <roman23kl@gmail.com>
2025-05-06 12:36:19 +0300

    upd. prisma schema

---
+++ config/prisma/migrations/20250506093603_del/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dateValue` on the `Sticker` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Sticker" DROP COLUMN "dateValue";
