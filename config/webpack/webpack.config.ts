8be97e89305092a4dddbc59ffeb4815ece29a1ba
beforedisappear <roman23kl@gmail.com>
2025-05-18 22:45:23 +0300

    fix docker compose, upd. scripts, +env.example, fix aliases

--- config/webpack/webpack.config.ts
+++ config/webpack/webpack.config.ts
@@ -1,5 +1,5 @@
 import type { NextJsWebpackConfig } from 'next/dist/server/config-shared';
-
+import path from 'path';
 export const WebpackConfig: NextJsWebpackConfig = function (config) {
   config.resolve.extensions.push('.svgr');
 
@@ -7,6 +7,14 @@ export const WebpackConfig: NextJsWebpackConfig = function (config) {
     test: /\.svgr$/i,
     loader: '@svgr/webpack',
   });
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, '../../src'),
+    'api/': path.resolve(__dirname, '../../api'),
+    'app/': path.resolve(__dirname, '../../app'),
+    'config/': path.resolve(__dirname, '../../config'),
+  };
 
   return config;
 };
